919450c35f18bde33fb5f5d55267b5d9bffe5908
Leo <leonoragilmour@sky.com>
2019-11-12 12:01:38 +0000

    adding jquery material

---
+++ script.js
@@ -0,0 +1,23 @@
+myFunction = function() {
+  $("h1").fadeOut("slow");
+  $("h1").fadeIn("slow");
+  $("div.icon-divider").css("cursor", "pointer");
+  $("div.icon-divider").on("click", function() {
+    console.log("You've clicked me!");
+    $(this)
+      .animate({
+        left: "250px",
+        opacity: "0.5",
+        height: "150px",
+        width: "150px"
+      })
+      .slideUp(2000)
+      .slideDown(2000)
+      .animate({
+        left: "0",
+        opacity: "1",
+        height: "33px",
+        width: "100%"
+      });
+  });
+};
